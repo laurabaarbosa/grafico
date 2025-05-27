af77f96f30a95d60c8851bc98ec4309cad207091
laurabaarbosa <lauraacademico2006@gmail.com>
2025-05-27 15:35:12 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import streamlit as st
 from matplotlib import pyplot as plt
+
 st.title("Gerar Gráfico a Partir de Arquivo CSV")
+
+
 df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
+
+
 st.subheader("Dados Carregados")
 st.write(df.head())
 
+
 colunas = df.columns.tolist()
-    x_col = st.selectbox("Escolha a coluna para o eixo X (categorias)", colunas)
-    y_col = st.selectbox("Escolha a coluna para o eixo Y (valores)", colunas)
+x_col = st.selectbox("Escolha a coluna para o eixo X (categorias)", colunas)
+y_col = st.selectbox("Escolha a coluna para o eixo Y (valores)", colunas)
+
 
 st.subheader("Gráfico de Barras")
-    st.bar_chart(data=df, x=x_col, y=y_col, use_container_width=True)
+st.bar_chart(data=df, x=x_col, y=y_col, use_container_width=True)

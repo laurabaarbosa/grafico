f8403e3c3e97f6846764603ee030bb6f839a403a
laurabaarbosa <lauraacademico2006@gmail.com>
2025-05-27 15:32:14 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -7,8 +7,8 @@ st.subheader("Dados Carregados")
 st.write(df.head())
 
 colunas = df.columns.tolist()
-    x_col = st.selectbox("Escolha a coluna X", colunas)
-    y_col = st.selectbox("Escolha a coluna Y", colunas)
+    x_col = st.selectbox("Escolha a coluna para o eixo X (categorias)", colunas)
+    y_col = st.selectbox("Escolha a coluna para o eixo Y (valores)", colunas)
 
 st.subheader("Gráfico de Barras")
     st.bar_chart(data=df, x=x_col, y=y_col, use_container_width=True)

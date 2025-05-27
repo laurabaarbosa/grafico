ca4863012642961803ef13356f80e41ad53ac57b
laurabaarbosa <lauraacademico2006@gmail.com>
2025-05-27 15:28:38 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import streamlit as st
 from matplotlib import pyplot as plt
-df['total'].plot(kind='line', figsize=(8, 4), title= 'Total de Usuarios',)
-plt.gca().spines[['right']].set_visible(False)
+st.title("Gerar Gráfico a Partir de Arquivo CSV")
+df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
+st.subheader("Dados Carregados")
+st.write(df.head())
+
+colunas = df.columns.tolist()
+    x_col = st.selectbox("Escolha a coluna X", colunas)
+    y_col = st.selectbox("Escolha a coluna Y", colunas)
+
+st.subheader("Gráfico de Barras")
+    st.bar_chart(data=df, x=x_col, y=y_col, use_container_width=True)
